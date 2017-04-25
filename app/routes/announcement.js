a58761ad5a3c52eec9bcde2ac9e277f9cc9d863f
Grace Stuart and Jahan Walsh <grace@>
2017-04-25 10:44:46 -0700

    add announcement js to models routes and templates hbs component.

--- app/routes/announcement.js
+++ app/routes/announcement.js
@@ -1,6 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model()
-  //this is where save/destroy actions will go
+  model() {
+    return this.store.findAll('announcement');
+  },
+
+  actions: {
+    saveAnnouncement(params) {
+      var newAnnouncement = this.store.createAnnouncement('announcement', params);
+      newAnnouncement.save();
+      this.transitionTo('announcement');
+    },
+
+    destroyAnnouncement(announcement) {
+      announcement.destroyAnnouncement();
+      this.transitionTo('announcement');
+    }
+  }
 });
