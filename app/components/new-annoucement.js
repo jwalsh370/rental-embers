c3088d76718bbbf2bfb0c38571e9b05b44dc55da
Grace Stuart and Jahan Walsh <grace@>
2017-04-25 11:30:07 -0700

    add actions to new announcement js file

--- app/components/new-annoucement.js
+++ app/components/new-annoucement.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewAnnouncement: false,
+  actions: {
+    announcementFormShow() {
+      this.set('addNewAnnouncement', true);
+    },
+
+    saveAnnouncement1() {
+      var params = {
+        messageType: this.get('messageType'),
+        message: this.get('message')
+      };
+      this.set('addNewAnnouncement', false);
+      this.sendAction('saveAnnouncement2', params);
+    }
+  }
 });
